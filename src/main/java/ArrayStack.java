21f6d44bc5db565b5377c380a687864dfc3e7165
capeh <capehenrykid@gmail.com>
2023-01-31 19:40:14 -0500

    Moved and Added HW

---
+++ src/main/java/ArrayStack.java
@@ -0,0 +1,133 @@
+import java.util.NoSuchElementException;
+
+/**
+ * Your implementation of an ArrayStack.
+ *
+ * @author YOUR NAME HERE
+ * @version 1.0
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class ArrayStack<T> {
+
+    /*
+     * The initial capacity of the ArrayStack.
+     *
+     * DO NOT MODIFY THIS VARIABLE.
+     */
+    public static final int INITIAL_CAPACITY = 9;
+
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private T[] backingArray;
+    private int size;
+
+    /**
+     * Constructs a new ArrayStack.
+     */
+    public ArrayStack() {
+        this.backingArray = (T[]) new Object[INITIAL_CAPACITY];
+        this.size = 0;
+    }
+
+    /**
+     * Adds the data to the top of the stack.
+     *
+     * If sufficient space is not available in the backing array, resize it to
+     * double the current length.
+     *
+     * Must be amortized O(1).
+     *
+     * @param data the data to add to the top of the stack
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void push(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("You can not insert null!");
+        }
+        T[] copy = (T[]) new Object[(size == backingArray.length) ? 2 * backingArray.length : backingArray.length];
+        for (int i = 0; i < size; i++) {
+            copy[i] = backingArray[i];
+        }
+        backingArray = copy;
+        copy[size] = data;
+        size++;
+    }
+
+    /**
+     * Removes and returns the data from the top of the stack.
+     *
+     * Do not shrink the backing array.
+     *
+     * Replace any spots that you pop from with null.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the top of the stack
+     * @throws java.util.NoSuchElementException if the stack is empty
+     */
+    public T pop() {
+        if (size == 0) {
+            throw new NoSuchElementException("The Stack is Empty!");
+        }
+        T removalNode = backingArray[size - 1];
+        backingArray[size - 1] = null;
+        size--;
+        return removalNode;
+    }
+
+    /**
+     * Returns the data from the top of the stack without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data from the top of the stack
+     * @throws java.util.NoSuchElementException if the stack is empty
+     */
+    public T peek() {
+        if (size == 0) {
+            throw new NoSuchElementException("The Stack is Empty!");
+        }
+        return backingArray[size - 1];
+    }
+
+    /**
+     * Returns the backing array of the stack.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the backing array of the stack
+     */
+    public T[] getBackingArray() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingArray;
+    }
+
+    //TODO: Delete me!
+    public String toString() {
+        String s = "ArrayStack (" + size + "): ";
+        for (int i = 0 ; i < backingArray.length; i++) {
+            s += " " + backingArray[i];
+        }
+        return s;
+    }
+
+    /**
+     * Returns the size of the stack.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the stack
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -1,20 +1,22 @@
-import org.junit.Before;
-
 public class MyTest {
-
     public static void main(String[] args) {
-        CircularSinglyLinkedList<String> list = new CircularSinglyLinkedList<>();
-        list.addAtIndex(0, "0a");   // 0a
-        list.addAtIndex(1, "1a");   // 0a, 1a
-        list.addAtIndex(2, "2a");   // 0a, 1a, 2a
-        list.addAtIndex(3, "3a");   // 0a, 1a, 2a, 3a
-        list.addAtIndex(4, "4a");   // 0a, 1a, 2a, 3a, 4a
-        list.addAtIndex(5, "5a");   // 0a, 1a, 2a, 3a, 4a, 5a
-        System.out.println(list.toString());
-        list.removeFromFront();
-        System.out.println(list.toString());
-        list.removeFromBack();
-        System.out.println(list.toString());
-        list.removeFromFront();
+        ArrayQueue<Integer> test = new ArrayQueue<>();
+        for (int i = 0; i < 9; i++) {
+            test.enqueue(i);
+        }
+        System.out.println(test.toString());
+        test.dequeue();
+        test.dequeue();
+        System.out.println(test.toString());
+        test.enqueue(61365);
+        test.enqueue(12);
+        System.out.println(test.toString());
+
+        test.dequeue();
+        System.out.println(test.toString());
+        test.enqueue(13);
+        System.out.println(test.toString());
+        test.enqueue(14);
+        System.out.println(test.toString());
     }
 }
